d4b4f318d9740f765b2d6df3d3df91e5ad5dfb0d
ZISJ007 <ttlqkdlsn007@naver.com>
2025-05-23 15:50:17 +0900

    Update JumpPad.cs

--- Assets/Scripts/JumpPad/JumpPad.cs
+++ Assets/Scripts/JumpPad/JumpPad.cs
@@ -17,22 +17,23 @@ public class JumpPad : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (!isReady || !collision.gameObject.CompareTag("Player")) return;
+        // 점프 패드가 재활성화 되지 않았거나 충돌한 오브젝트가 플레이어가 아닐 경우
+        if (!isReady || !collision.gameObject.CompareTag("Player")) return; 
 
         if (collision.gameObject.CompareTag("Player"))
         {
             Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
             if (rb != null)
             {
-                rb.AddForce(jumpForce, ForceMode.Impulse);
+                rb.AddForce(jumpForce, ForceMode.Impulse);// 점프 패드에 충돌한 플레이어에게 힘을 가함
                 StartCoroutine(JumpPadDelayCoroutine());
             }
         }
     }
     private IEnumerator JumpPadDelayCoroutine()
     {
-        isReady = false;
-        jumpPadCol.enabled = false;
+        isReady = false; // 점프 패드 비활성화
+        jumpPadCol.enabled = false; // 점프 패드의 콜라이더 비활성화
         yield return new WaitForSeconds(jumpDelay);
         jumpPadCol.enabled = true;
         isReady = true;
